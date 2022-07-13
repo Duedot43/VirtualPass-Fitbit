b963ec44ff46db85d353511b920526cc025541dd
Duedot43 <gendillj@gmail.com>
2022-07-13 11:21:35 -0600

    Login page coming up

--- app/index.js
+++ app/index.js
@@ -3,24 +3,35 @@ import { getElementById } from "document";
 import { existsSync } from "fs";
 import { writeFileSync } from "fs";
 import { readFileSync } from "fs";
+import { me as companion } from "companion";
 
 //console.log(existsSync("/private/data/info.json") + "  WEEEEEEEE OOOO");
-if (!existsSync("/private/data/info.json")){
+if (!existsSync("/private/data/info.json")) {
     writeFile("/private/data/info.json", '{"hostname":"", "key":""}');
     console.log("File write");
 }
 
+const val = {
+    "method": "method",
+    "data": "data",
+    "url": "url"
+}
+
+setTimeout(() => { request("GET", val, "https://google.com", "fetch"); }, 3000);
+
+
 let activ = getElementById("activ");
 
 let button = getElementById("button");
-button.onactivate = function(evt) {
-  button.text = "Clicked";
+button.onactivate = function (evt) {
+    button.text = "Clicked";
 }
 
 
 //let myElement = document.getElementById("myElement");
 //Trigger the event if the settings are changed
 messaging.peerSocket.addEventListener("message", (evt) => {
+    console.log("Ready to recive messages")
     if (evt && evt.data && evt.data.key === "URL") {
         //evt.data.value is the value of the setting URL
         var infoJson = JSON.parse(readFile("/private/data/info.json"));
@@ -35,11 +46,38 @@ messaging.peerSocket.addEventListener("message", (evt) => {
         writeFile("/private/data/info.json", JSON.stringify(infoJson));
         console.log(readFile("/private/data/info.json"));
     }
+    if (evt && evt.data && evt.data.key === "request") {
+        var recive = evt.data.value;
+    }
 });
 function readFile(path) {
     let ascii_read = readFileSync(path, "ascii");
     return ascii_read;
 }
-function writeFile(path, content){
+function writeFile(path, content) {
     writeFileSync(path, content, "ascii");
+}
+function request(method, data, url, value) {
+    const val = {
+        method: method,
+        data: data,
+        url: url
+    }
+    sendValue(value, JSON.stringify(val))
+}
+function sendValue(key, val) {
+    if (val) {
+        sendSettingData({
+            key: key,
+            value: JSON.parse(val)
+        });
+    }
+}
+function sendSettingData(data) {
+    // If we have a MessageSocket, send the data to the device
+    if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+        messaging.peerSocket.send(data);
+    } else {
+        console.log("No peerSocket connection");
+    }
 }
--- companion/index.js
+++ companion/index.js
@@ -4,6 +4,36 @@ import { me as companion } from "companion";
 
 let KEY_COLOR = "URL";
 
+
+var URL = settingsStorage.getItem("URL");
+var KEY = settingsStorage.getItem("key");
+if (URL != "" && KEY != ""){
+  userData = request("GET", "{}", "https://" + KEY + "@" + URL + "/api_student/student/");
+  console.log(userData)
+}
+
+messaging.peerSocket.addEventListener("message", (evt) => {
+  console.log("Ready to recive messages")
+  if (evt && evt.data && evt.data.key === "fetch") {
+    console.log("Recived");
+  }
+});
+
+function request(method, data, url) {
+  let options = {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(data)
+  }
+  var response = fetch(url, options);
+  response.then(res =>
+    res.json()).then(d => {
+      return d
+    })
+}
+
 // Settings have been changed
 settingsStorage.addEventListener("change", (evt) => {
   sendValue(evt.key, evt.newValue);
@@ -12,7 +42,7 @@ settingsStorage.addEventListener("change", (evt) => {
 // Settings were changed while the companion was not running
 if (companion.launchReasons.settingsChanged) {
   // Send the value of the setting
-  //sendValue(KEY_COLOR, settingsStorage.getItem(KEY_COLOR));
+  sendValue(KEY_COLOR, settingsStorage.getItem(KEY_COLOR));
 }
 
 function sendValue(key, val) {
